15e031080a01c2e7237f90c39a3b835d74cdba74
MarianaBarretoCas <marianabarretocas@gmail.com>
2026-06-03 22:43:51 -0500

    Adding navbar

--- Portfolio/src/components/Navbar.vue
+++ Portfolio/src/components/Navbar.vue
@@ -1,3 +1,23 @@
-<nav class="">
-    
-</nav>
+<template>
+    <header class="sticky top-0 z-50 bg-slate-950/80 backdrop-blur border-b border-white/10">    
+        
+        <nav class="navbar max-w-7xl mx-auto py-4 px-6">
+            <div class="flex items-center justify-between w-full">
+                <div class="logo">
+                    <a href="#home" class="font-logo text-3xl font-bold text-white">Mariana Barreto</a><span class="text-sm text-gray-400">.dev</span>
+                </div>
+                <ul class="nav-links hidden md:flex items-center gap-8 text-sm text-slate-300 font-medium text-gray-300 transition">
+                    <li><a href="#home" class="hover:text-violet-400 transition-colors">Home</a></li>
+                    <li><a href="#about" class="hover:text-violet-400 transition-colors">About</a></li>
+                    <li><a href="#skills" class="hover:text-violet-400 transition-colors">Skills</a></li>
+                    <li><a href="#projects" class="hover:text-violet-400 transition-colors">Projects</a></li>
+                    <li><a href="#contact" class="hover:text-violet-400 transition-colors">Contact</a></li>
+                </ul>
+                <div class="buttons hidden md:flex items-center gap-4">
+                    <a href="#download-cv" class="bg-gradient-to-r from-purple-700 to-violet-700 hover:from-purple-500 hover:to-violet-500 text-white font-medium py-2 px-6 rounded-xl shadow-md transition-all">Download CV</a>
+                    <button class="border border-white/10 rounded-xl px-3 py-2">🌙</button>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
--- Portfolio/src/views/HomeView.vue
+++ Portfolio/src/views/HomeView.vue
@@ -1,42 +1,16 @@
+<script setup>
+import Navbar from '../components/Navbar.vue'
+</script>
+
 <template>
-  <main class="min-h-screen bg-slate-950 text-white">
-    <section id="home" class="min-h-screen flex items-center justify-center">
-      <h1 class="text-5xl font-bold text-purple-500">
-        Portafolio Mariana
-      </h1>
-    </section>
-
-    <section id="about" class="min-h-screen flex items-center justify-center">
-      <h2 class="text-4xl font-bold">
-        About Me
-      </h2>
-    </section>
-
-    <section id="skills" class="min-h-screen flex items-center justify-center">
-      <h2 class="text-4xl font-bold">
-        Skills
-      </h2>
-    </section>
-
-    <section id="projects" class="min-h-screen flex items-center justify-center">
-      <h2 class="text-4xl font-bold">
-        Projects
-      </h2>
-    </section>
-
-    <section id="experience" class="min-h-screen flex items-center justify-center">
-      <h2 class="text-4xl font-bold">
-        Experience
-      </h2>
-    </section>
-
-    <section id="contact" class="min-h-screen flex items-center justify-center">
-      <h2 class="text-4xl font-bold">
-        Contact
-      </h2>
-    </section>
+  <div id="body" class="min-h-screen bg-slate-950 text-white">
+    <navbar />
+  <main>
+    hola
   </main>
   <footer class="bg-slate-900 text-center py-4">
     <p>&copy; 2024 Mariana. All rights reserved.</p>
     </footer>
+  </div>
+  
 </template>
